b4bb2fe58d5a1baf6d17392814992b41b3494a18
samuelcpr <samuelcpr464@gmail.com>
2022-11-20 01:06:25 -0300

    add string function

---
+++ Métodos/index02.rb
@@ -0,0 +1,14 @@
+def talk(frist_name, last_name)
+    puts "Olá,#{frist_name} #{last_name} como voce está ?"
+    puts 'Voce gosta de programação?'
+end
+
+talk('Samuel', 'Carlos')
+talk('Vitor', 'Carlos')
+
+=begin
+O metodo talk recebe os  o conteudo que vai ser imprimido
+ e os parametros frist_name e last_name
+
+
+=end
