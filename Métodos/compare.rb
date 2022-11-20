f414824147c5c9f293f5b390c2e4f63262e92dd6
samuelcpr <samuelcpr464@gmail.com>
2022-11-20 01:04:49 -0300

    git commit -m "first commit"

---
+++ Métodos/compare.rb
@@ -0,0 +1,13 @@
+def compare(a, b)
+    a > b 
+end
+
+frist_value = 1
+second_value = 2
+
+# atribuindo valor a aos parametros a e b
+result = compare(frist_value, second_value)
+
+puts "O resultado da comparação é #{result}"
+
+# O resultado é falso
